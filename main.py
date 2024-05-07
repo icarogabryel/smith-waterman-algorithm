edbde9350c3506cc0e5fb85f59973fbf4fd0fc82
Ícaro Gabryel <icarogabryel2001@gmail.com>
2024-05-07 14:57:23 -0300

    output file info update

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from ScoreMatrix import ScoreMatrix
 
 def main():
+    # Read input from file
     with open('input.txt', 'r') as file:
         vSeq = file.readline().strip()
         hSeq = file.readline().strip()
@@ -9,19 +10,28 @@ def main():
         missScore = int(file.readline())
         matchScore = int(file.readline())
     
+    # Create score matrix
     matrix = ScoreMatrix(matchScore, missScore, gapScore, vSeq, hSeq)
 
+    # Print alignments and score matrix
     print('\nAlignments found\n')
     for i in matrix.getBiggestAlignments():
         print('-' * 30, '\n')
         print(i[0], '\n')
         print(matrix.getMatrixInStr(i[1]), '\n')
 
+    # Write output to file
     with open('output.txt', 'w') as file:
-        file.write('Score Matrix\n\n')
+        file.write(f'{'-' * 30}\n')
+        file.write('Valores de score\n')
+        file.write(f'{'-' * 30}\n')
+        
         file.write(matrix.getMatrixInStrNoColor() + '\n')
 
-        file.write('Alignments found\n\n')
+        file.write(f'{'-' * 30}\n')
+        file.write(f'Alinhamentos ( Match = {matchScore} | Missmatch = {missScore} | Gap = {gapScore})\n')
+        file.write(f'{'-' * 30}\n')
+
         for i in matrix.getBiggestAlignments():
             file.write(i[0] + '\n\n')
 
